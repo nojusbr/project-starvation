5f975ee4e0f465b50e4a7bf49efd5c798aaa8cbe
nojusbr <100302883+nojusbr@users.noreply.github.com>
2024-01-07 19:22:36 +0200

    dashing and movement reworked

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class FirstPersonMovement : MonoBehaviour
 {
     public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
     public float runSpeed = 9;
     public KeyCode runningKey = KeyCode.LeftShift;
+    Rigidbody rigidbody;
+
+    [Header("References")]
+    public Transform orientation;
+    public Transform playerCam;
+    public ParticleSystem dashEffect;
+
+    [Header("Dashing")]
+    public float dashForce;
+    public float dashDuration;
+
+    [Header("Cooldown")]
+    public float dashCd;
+    private float dashCdTimer;
+
+    [Header("Input")]
     public KeyCode dashKey = KeyCode.Space;
 
-    public bool canRun = true;
-    public bool canDash = true;
-    public bool IsRunning { get; private set; }
-    public bool IsNotDashing { get; private set; }
-
-    public float dashSpeed = 15f; // Adjust the speed of the dash as needed
-    public float dashDuration = 1f; // Duration of the dash in seconds
-
-    private bool isDashing = false;
-    private float dashTimer = 0f;
-    private Rigidbody rigidbody;
-
-    public ParticleSystem dashEffect;
 
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
@@ -34,53 +41,43 @@ public class FirstPersonMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        Move();
+
+        if (Input.GetKeyDown(dashKey))
+        {
+            dashEffect.Play();
+            Dash();
+        }
+
+
+        if (dashCdTimer > 0)
+            dashCdTimer -= Time.deltaTime;
+    }
+
+    private void Move()
+    {
         // Update IsRunning from input.
         IsRunning = canRun && Input.GetKey(runningKey);
-        IsNotDashing = canDash && Input.GetKey(dashKey);
 
-        // Check if the player initiates a dash
-        if (IsNotDashing && Input.GetKeyDown(dashKey) && !isDashing)
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
         {
-            isDashing = true;
-            dashTimer = dashDuration;
+            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
         }
 
-        // Perform the dash
-        if (isDashing)
-        {
-            // Reduce the timer for the dash duration
-            dashTimer -= Time.deltaTime;
-            dashEffect.Play();
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
 
+        // Apply movement.
+        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+    }
 
-            // Check if the dash duration has elapsed
-            if (dashTimer <= 0f)
-            {
-                isDashing = false;
-                dashTimer = 0f;
-
-            }
-
-            // Calculate the velocity for dash movement
-            Vector3 dashVelocity = transform.forward * dashSpeed;
-
-            // Apply the dash movement
-            rigidbody.velocity = dashVelocity;
-        }
-        else
-        {
-            // Get targetMovingSpeed.
-            float targetMovingSpeed = IsRunning ? runSpeed : speed;
-            if (speedOverrides.Count > 0)
-            {
-                targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
-            }
-
-            // Get targetVelocity from input.
-            Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
-
-            // Apply regular movement if not dashing
-            rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
-        }
+    private void Dash()
+    {
+        if (dashCdTimer > 0) return;
+        else dashCdTimer = dashCd;
+        Vector3 forceToApply = orientation.forward * dashForce + orientation.up;
+        rigidbody.AddForce(forceToApply, ForceMode.Impulse);
     }
 }
